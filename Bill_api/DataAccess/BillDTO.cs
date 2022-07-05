814a36ebb6fc3fb587cf9f79b145efe647b2f441
unknown <gokuruto1234@gmail.com>
2022-07-05 21:35:25 +0200

    gitignore

--- Bill_api/DataAccess/BillDTO.cs
+++ Bill_api/DataAccess/BillDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bill_api.Database.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Bill_api.DataAccess
 {
     public class BillDTO
     {
+        private BillContext _billDB;
+
+        public BillDTO(BillContext billDB)
+        {
+            _billDB = billDB;
+        }
     }
 }
